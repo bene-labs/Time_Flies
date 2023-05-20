d0b73aeb86233614748b5e43676c705328cd9d96
G0vinda <frederickfreund@gmail.com>
2023-05-20 19:44:30 +0200

    Add SoundPlayer functions

--- Assets/Scripts/SoundPlayer.cs
+++ Assets/Scripts/SoundPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using FMOD.Studio;
@@ -7,7 +8,9 @@ using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 public class SoundPlayer : MonoBehaviour
 {
-    [SerializeField] private EventReference music;
+    [SerializeField] private EventReference music1;
+    [SerializeField] private EventReference music2;
+    [SerializeField] private EventReference music3;
     
     [SerializeField] private EventReference eat;
     [SerializeField] private EventReference fly;
@@ -17,9 +20,23 @@ public class SoundPlayer : MonoBehaviour
     [SerializeField] private EventReference item_spawn;
     [SerializeField] private EventReference SFX_Failed;
     [SerializeField] private EventReference SFX_Success;
-    
-    
+
+    public static SoundPlayer Instance;
     private EventInstance musicInstance;
+    private EventInstance flySoundInstance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
         //RuntimeManager.PlayOneShot();
@@ -29,6 +46,37 @@ public class SoundPlayer : MonoBehaviour
     {
         musicInstance.stop(STOP_MODE.ALLOWFADEOUT);
     }
+
+    public void PlayMusic1()
+    {
+        musicInstance.stop(STOP_MODE.IMMEDIATE);
+        musicInstance = RuntimeManager.CreateInstance(music1);
+        musicInstance.start();
+    }
     
+    public void PlayMusic2()
+    {
+        musicInstance.stop(STOP_MODE.IMMEDIATE);
+        musicInstance = RuntimeManager.CreateInstance(music2);
+        musicInstance.start();
+    }
     
+    public void PlayMusic3()
+    {
+        musicInstance.stop(STOP_MODE.IMMEDIATE);
+        musicInstance = RuntimeManager.CreateInstance(music3);
+        musicInstance.start();
+    }
+
+    public void StartFlySound()
+    {
+        flySoundInstance.stop(STOP_MODE.IMMEDIATE);
+        //flySoundInstance = RuntimeManager.CreateInstance()
+        flySoundInstance.start();
+    }
+
+    public void StopFlySound()
+    {
+        flySoundInstance.stop(STOP_MODE.ALLOWFADEOUT);
+    }
 }
