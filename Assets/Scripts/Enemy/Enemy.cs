a574f546e0607c9d6471ced21c7540e468587c15
Marco Perschke <Marco.Perschke@Student.HTW-Berlin.de>
2023-05-21 04:14:20 +0200

    Add enemies now face towards their movement direction

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,10 +5,14 @@ namespace Enemy
     public class Enemy : MonoBehaviour
     {
         public GameObject target;
+        public GameObject spriteObject;
+        public float spriteRotationOffset = 180;
+        public float rotationSpeed = 20;
         private float moveSpeed;
         public float maxSpeed;
         public float minSpeed;
         public float energyDrain;
+
 
         private void Start()
         {
@@ -17,7 +21,16 @@ namespace Enemy
 
         private void Update()
         {
-            transform.position = Vector2.Lerp(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+            Vector3 oldPosition = transform.position;
+            Vector3 newPosition = Vector3.Lerp(oldPosition, target.transform.position, moveSpeed * Time.deltaTime);
+
+            transform.position = newPosition;
+
+            // rotate enemy to face the movement direction
+            Vector3 movementDirection = newPosition - oldPosition;
+            float angle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.Euler(new Vector3(0, 0, angle + spriteRotationOffset));
+            spriteObject.transform.rotation = Quaternion.Slerp(spriteObject.transform.rotation, rotation, rotationSpeed * Time.deltaTime);
         }
     }
 }
